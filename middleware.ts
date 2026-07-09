9aeba33ae9877df6e4a6ea0c3026394db1e9bd4f
Pankaj862 <Pankaj721780@gmail.com>
2026-07-09 20:25:42 +0530

    database connection

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import {NextRequest, NextResponse } from 'next/server'
 
-export {default} from 'next-auth/middleware'
+
 import { getToken } from 'next-auth/jwt'
  
 // This function can be marked `async` if using `await` inside
@@ -8,7 +8,7 @@ export async function middleware(request: NextRequest)  {
 
   const token = await getToken({req: request})
   const url = request.nextUrl
-  if(token && (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up') || url.pathname.startsWith('/'))) {
+  if(token && (url.pathname.startsWith('/sign-in') || url.pathname.startsWith('/sign-up'))) {
   return NextResponse.redirect(new URL('/dashboard', request.url))
 }
   if(!token && (url.pathname.startsWith('/dashboard'))) {
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,25 +1,31 @@
-import { promises } from "dns";
 import mongoose from "mongoose";
 
 type ConnectionObject = {
-    isConnected?: number,
+    isConnected?: number;
 }
 
-const connection: ConnectionObject = {}
+const connection: ConnectionObject = {};
 
 async function dbConnect(): Promise<void> {
-    if(connection.isConnected) {
-        console.log("db already connected")
-        return
+    if (connection.isConnected) {
+        console.log("Database already connected");
+        return;
     }
-    try{
-      const db=  await mongoose.connect(process.env.MONGODB_URI || '')
-      connection.isConnected = db.connections[0].readyState
-      console.log("Db connected succesfully")
+
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+        throw new Error("MONGODB_URI is not defined in environment variables");
     }
-    catch(error){
-        console.log("db connection failed", error)
-        process.exit(1)
+
+    try {
+        const db = await mongoose.connect(uri);
+        connection.isConnected = db.connections[0].readyState;
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        
+        throw error;
     }
 }
-export default dbConnect
+
+export default dbConnect;
